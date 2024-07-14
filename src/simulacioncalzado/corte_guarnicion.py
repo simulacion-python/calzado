530ea59be48bdb4e128a7aed8e3e022e54a0d4c6
Luis <luis@10.10.5.119>
2024-07-14 18:42:05 -0500

    crear clase de corte

--- src/simulacioncalzado/corte_guarnicion.py
+++ src/simulacioncalzado/corte_guarnicion.py
@@ -1,3 +1,13 @@
 """
 Funciones necesarias para simular el corte y la guarnicion
 """
+import simpy
+import random
+import pandas as pd
+from scipy.stats import norm
+
+from .hiperparametros import Hiperparametros
+
+class Corte_Guarnicion():
+  def __init__(self,env,cortes,df_metricas,df_estado,pipe={},estado={}):
+    pass
